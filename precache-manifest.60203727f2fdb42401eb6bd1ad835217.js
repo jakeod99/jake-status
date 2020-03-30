5d200cf4b1acd29fc768940439f2bfae599ba666
Jake O'Donnell <jakeod99@gmail.com>
2020-03-30 18:17:21 -0400

    Updates

---
+++ precache-manifest.60203727f2fdb42401eb6bd1ad835217.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f21e02ea4cda8eccf161b7eaffadb2ff",
+    "url": "/jake-status/index.html"
+  },
+  {
+    "revision": "4da05f0aae40d27073b7",
+    "url": "/jake-status/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d5838f53060943092c55",
+    "url": "/jake-status/static/js/2.9a0129e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jake-status/static/js/2.9a0129e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4da05f0aae40d27073b7",
+    "url": "/jake-status/static/js/main.21113787.chunk.js"
+  },
+  {
+    "revision": "2af548a3621515da27e5",
+    "url": "/jake-status/static/js/runtime-main.3a4917a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jake-status/static/media/logo.5d5d9eef.svg"
+  }
+]);
